1b9d76c0ec72fdd390a392f3dc2fdebe0aff28ac
Chris Marslender <chrismarslender@gmail.com>
2017-09-15 18:11:25 -0700

    Super basic table test

---
+++ tests/Tables/PostToUserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TenUp\P2P\Tests\Tables;
+
+class PostToUserTest extends \PHPUnit_Framework_TestCase {
+
+	public function test_table_is_created() {
+		global $wpdb;
+
+		do_action( 'admin_init' );
+
+		$result = $wpdb->query( "SHOW TABLES LIKE '{$wpdb->prefix}post_to_user'" );
+
+		$this->assertEquals( 1, $result );
+	}
+
+}
