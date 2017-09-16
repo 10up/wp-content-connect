abbcdf4c3ffcb45dc934ec6316f524b6b3c02a7c
Chris Marslender <chrismarslender@gmail.com>
2017-09-15 19:53:24 -0700

    Add tests for the registry

--- tests/RegistryTest.php
+++ tests/RegistryTest.php
@@ -4,6 +4,7 @@ namespace TenUp\P2P\Tests;
 
 use TenUp\P2P\Registry;
 use TenUp\P2P\Relationships\PostToPost;
+use TenUp\P2P\Relationships\PostToUser;
 
 class RegistryTest extends P2PTestCase {
 
@@ -15,15 +16,17 @@ class RegistryTest extends P2PTestCase {
 		$registry = new Registry();
 
 		$this->assertFalse( $registry->post_to_post_relationship_exists( 'post', 'post', 'basic' ) );
+		$this->assertFalse( $registry->post_to_user_relationship_exists( 'post', 'owner' ) );
 	}
 
 	public function test_relationship_can_be_added() {
 		$registry = new Registry();
 
 		$this->assertInstanceOf( PostToPost::class, $registry->define_post_to_post( 'post', 'post', 'basic' ) );
+		$this->assertInstanceOf( PostToUser::class, $registry->define_post_to_user( 'post', 'owner' ) );
 	}
 
-	public function test_doesnt_add_duplicates() {
+	public function test_doesnt_add_duplicate_post_to_post_relationship() {
 		$registry = new Registry();
 
 		$this->expectException( \Exception::class );
@@ -32,11 +35,23 @@ class RegistryTest extends P2PTestCase {
 		$registry->define_post_to_post( 'post', 'post', 'basic' );
 	}
 
+	public function test_doesnt_add_duplicate_post_to_user_relationship() {
+		$registry = new Registry();
+
+		$this->expectException( \Exception::class );
+
+		$registry->define_post_to_user( 'post', 'owner' );
+		$registry->define_post_to_user( 'post', 'owner' );
+	}
+
 	public function test_can_define_different_types_for_same_cpts() {
 		$registry = new Registry();
 
 		$this->assertInstanceOf( PostToPost::class, $registry->define_post_to_post( 'post', 'post', 'type1' ) );
 		$this->assertInstanceOf( PostToPost::class, $registry->define_post_to_post( 'post', 'post', 'type2' ) );
+
+		$this->assertInstanceOf( PostToUser::class, $registry->define_post_to_user( 'post', 'owner' ) );
+		$this->assertInstanceOf( PostToUser::class, $registry->define_post_to_user( 'post', 'contrib' ) );
 	}
 
 	public function test_flipped_order_is_still_duplicate() {
@@ -48,7 +63,7 @@ class RegistryTest extends P2PTestCase {
 		$registry->define_post_to_post( 'car', 'post', 'basic' );
 	}
 
-	public function test_retreival_of_relationship() {
+	public function test_retreival_of_post_to_post_relationships() {
 		$registry = new Registry();
 
 		// Add all the relationship types so we know we aren't just lucky in the return values
@@ -72,6 +87,21 @@ class RegistryTest extends P2PTestCase {
 
 		// Check that calling inverse args returns the same as well (it should, based on above two tests)
 		$this->assertSame( $registry->get_post_to_post_relationship( 'post', 'car', 'basic' ), $registry->get_post_to_post_relationship( 'car', 'post', 'basic' ) );
+	}
+
+	public function test_retreival_of_post_to_user_relationships() {
+		$registry = new Registry();
+
+		$po = $registry->define_post_to_user( 'post', 'owner' );
+		$pc = $registry->define_post_to_user( 'post', 'contrib' );
+
+		$pc2 = new PostToUser( 'post', 'contrib' );
+
+		// verify that two separate objects are NOT the same (sanity check)
+		$this->assertNotSame( $pc, $pc2 );
+
+		$this->assertSame( $po, $registry->get_post_to_user_relationship( 'post', 'owner' ) );
+		$this->assertSame( $pc, $registry->get_post_to_user_relationship( 'post', 'contrib' ) );
 	}
 
 	public function test_retreival_of_unique_types_on_same_cpt() {
