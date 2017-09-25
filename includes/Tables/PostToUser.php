faf46eb89b8fd38272467cb6926ed7405d2b7371
Chris Marslender <chrismarslender@gmail.com>
2017-09-25 10:47:17 -0700

    Add post_order column to user relationships

--- includes/Tables/PostToUser.php
+++ includes/Tables/PostToUser.php
@@ -5,7 +5,7 @@ namespace TenUp\ContentConnect\Tables;
 class PostToUser extends BaseTable {
 
 	function get_schema_version() {
-		return '0.1.5';
+		return '0.1.7';
 	}
 
 	function get_table_name() {
@@ -16,10 +16,11 @@ class PostToUser extends BaseTable {
 		$table_name = $this->get_table_name();
 
 		$sql = "CREATE TABLE `{$table_name}` ( 
-			`post_id` bigint(20) unsigned NOT NULL, 
-			`user_id` bigint(20) unsigned NOT NULL, 
-			`name` varchar(20) NOT NULL,
-			`user_order` int(11) NOT NULL default 0,
+			post_id bigint(20) unsigned NOT NULL, 
+			user_id bigint(20) unsigned NOT NULL, 
+			name varchar(20) NOT NULL,
+			user_order int(11) NOT NULL default 0,
+			post_order int(11) NOT NULL default 0,
 			UNIQUE KEY post_user_name (`post_id`,`user_id`,`name`)
 		);";
 
