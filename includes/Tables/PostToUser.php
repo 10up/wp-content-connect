0a699cb0c2982c826fb0770e96885c614fc6c104
Alexis Bellido <alexis@ventanazul.com>
2017-12-19 20:25:28 -0500

    make name column bigger or relationships will fail silently

--- includes/Tables/PostToUser.php
+++ includes/Tables/PostToUser.php
@@ -5,7 +5,7 @@ namespace TenUp\ContentConnect\Tables;
 class PostToUser extends BaseTable {
 
 	function get_schema_version() {
-		return '0.1.8';
+		return '0.1.10';
 	}
 
 	function get_table_name() {
@@ -27,10 +27,10 @@ class PostToUser extends BaseTable {
 	function get_schema() {
 		$table_name = $this->get_table_name();
 
-		$sql = "CREATE TABLE `{$table_name}` ( 
-			post_id bigint(20) unsigned NOT NULL, 
-			user_id bigint(20) unsigned NOT NULL, 
-			name varchar(20) NOT NULL,
+		$sql = "CREATE TABLE `{$table_name}` (
+			post_id bigint(20) unsigned NOT NULL,
+			user_id bigint(20) unsigned NOT NULL,
+			name varchar(64) NOT NULL,
 			user_order int(11) NOT NULL default 0,
 			post_order int(11) NOT NULL default 0,
 			UNIQUE KEY post_user_name (`post_id`,`user_id`,`name`),
