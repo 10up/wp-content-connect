7bc9d8bbd62915013d4b7d78041366fc73650db9
Chris Marslender <chrismarslender@gmail.com>
2018-03-21 15:07:04 -0700

    Fix header already send error in tests

--- tests/integration/Tables/PostToPostTest.php
+++ tests/integration/Tables/PostToPostTest.php
@@ -7,7 +7,8 @@ class PostToPostTest extends \PHPUnit_Framework_TestCase {
 	public function test_table_is_created() {
 		global $wpdb;
 
-		do_action( 'admin_init' );
+		// @ suppresses headers already sent errors
+		@do_action( 'admin_init' );
 
 		$result = $wpdb->query( "SHOW TABLES LIKE '{$wpdb->prefix}post_to_post'" );
 
