07168b70caf9ab66bc9ed4dae35b6a5a6d706b95
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2018-11-28 11:14:20 +0530

    Bumps version to 1.0.0

--- content-connect.php
+++ content-connect.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Content Connect
  * Plugin URI: https://github.com/10up/wp-content-connect
  * Description: WordPress library that enables direct relationships for posts to posts and posts to users.
- * Version: 0.0.0
+ * Version: 1.0.0
  * Author: Chris Marslender
  * Author URI:
  * License: MIT
