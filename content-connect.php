b5c659a211f4f38713f388a8c3a8035256437266
Sérgio Santos <me@s3rgiosan.com>
2025-02-07 12:30:02 +0000

    Update version

--- content-connect.php
+++ content-connect.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Content Connect
  * Plugin URI: https://github.com/10up/wp-content-connect
  * Description: WordPress library that enables direct relationships for posts to posts and posts to users.
- * Version: 1.5.0
+ * Version: 1.7.0
  * Author: Chris Marslender
  * Author URI:
  * License: GPLv3
@@ -13,7 +13,7 @@
  * @package ContentConnect
  */
 
-define( 'CONTENT_CONNECT_VERSION', '1.5.0' );
+define( 'CONTENT_CONNECT_VERSION', '1.7.0' );
 define( 'CONTENT_CONNECT_URL', plugin_dir_url( __FILE__ ) );
 define( 'CONTENT_CONNECT_PATH', plugin_dir_path( __FILE__ ) );
 
