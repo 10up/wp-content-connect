750dfdf25786955bc0ad11db8ae45895e9ddee90
Sérgio Santos <me@s3rgiosan.com>
2025-02-17 15:04:57 +0000

    Add context support for REST field

--- includes/API/V2/Post/Field/Relationships.php
+++ includes/API/V2/Post/Field/Relationships.php
@@ -34,7 +34,7 @@ class Relationships extends AbstractField {
 					'schema'          => array(
 						'description' => __( 'Lists all relationships associated with this post.', 'tenup-content-connect' ),
 						'type'        => 'array',
-						'context'     => array( 'view', 'edit' ),
+						'context'     => array( 'view', 'edit', 'embed' ),
 					),
 				)
 			);
@@ -46,22 +46,26 @@ class Relationships extends AbstractField {
 	 *
 	 * @since 1.7.0
 	 *
-	 * @param  array $post_data Raw post data from the REST API request.
-	 * @return array
+	 * @param array            $object_data Details of current object.
+	 * @param string           $field_name  Name of field.
+	 * @param \WP_REST_Request $request     Current request.
+	 * @return mixed
 	 */
-	public function get_relationships( $post_data ) {
+	public function get_relationships( $object_data, $field_name, $request ) {
 
-		if ( empty( $post_data['id'] ) ) {
+		if ( empty( $object_data['id'] ) ) {
 			return array();
 		}
 
-		$post = get_post( (int) $post_data['id'] );
+		$post = get_post( (int) $object_data['id'] );
 
 		if ( empty( $post ) || empty( $post->ID ) ) {
 			return array();
 		}
 
-		$relationships = get_post_relationship_data( $post );
+		$context = $request->get_param( 'context' );
+
+		$relationships = get_post_relationship_data( $post, 'any', false, $context );
 
 		return $relationships;
 	}
