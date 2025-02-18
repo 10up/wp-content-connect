a39e879e8d49c63592e6fe4bbd44b9322021c153
Fabian Kägy <mail@fabian-kaegy.de>
2025-02-18 11:55:04 +0100

    add initial version of UI

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,2 +1,9 @@
 import './store';
 import './hooks';
+
+import { registerPlugin } from '@wordpress/plugins';
+import { RelationshipsPanel } from './components/relationships-panel';
+
+registerPlugin('wp-content-connect', {
+	render: RelationshipsPanel,
+});
