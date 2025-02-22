8ebe06b6812b6eff1a7b4e6ec5c1ae3e6fd126ac
Sérgio Santos <me@s3rgiosan.com>
2025-02-22 00:29:14 +0000

    Register classic editor metaboxes

--- includes/UI/BlockEditor.php
+++ includes/UI/BlockEditor.php
@@ -4,11 +4,15 @@ namespace TenUp\ContentConnect\UI;
 
 /**
  * Class BlockEditor
+ *
+ * @package TenUp\ContentConnect\UI
  */
 class BlockEditor {
 
 	/**
-	 * Setup the class.
+	 * Setup the block editor module.
+	 *
+	 * @since 1.7.0
 	 */
 	public function setup() {
 		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_assets' ] );
@@ -16,18 +20,20 @@ class BlockEditor {
 
 	/**
 	 * Enqueue block editor assets.
+	 *
+	 * @since 1.7.0
 	 */
 	public function enqueue_block_editor_assets() {
-		$asset_info = require CONTENT_CONNECT_PATH . 'dist/js/wp-content-connect.asset.php';
+		$asset_info = require CONTENT_CONNECT_PATH . 'dist/js/block-editor.asset.php';
 
 		wp_register_script(
-			'wp-content-connect',
-			CONTENT_CONNECT_URL . 'dist/js/wp-content-connect.js',
+			'wp-content-connect-block-editor',
+			CONTENT_CONNECT_URL . 'dist/js/block-editor.js',
 			$asset_info['dependencies'],
 			$asset_info['version'],
 			true
 		);
 
-		wp_enqueue_script( 'wp-content-connect' );
+		wp_enqueue_script( 'wp-content-connect-block-editor' );
 	}
 }
