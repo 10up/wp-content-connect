fad2d96059781847fa38ae7ad25c2f69173cb6a0
Sérgio Santos <me@s3rgiosan.com>
2025-03-17 12:48:19 +0000

    Update query block extension

--- assets/js/block-extensions/core-query.js
+++ assets/js/block-extensions/core-query.js
@@ -1,13 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
 /**
  * External dependencies
  */
+import { v4 as uuidv4 } from 'uuid';
 import { registerBlockExtension, ContentPicker } from '@10up/block-components';
 
 /**
  * WordPress dependencies
  */
-import { ToggleControl, PanelBody, Notice } from '@wordpress/components';
+import {
+	ToggleControl,
+	PanelBody,
+	Notice,
+	SelectControl,
+	BaseControl,
+} from '@wordpress/components';
 import { InspectorControls } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
@@ -22,12 +30,19 @@ import { store } from '../store';
 
 const BlockEdit = ({ setAttributes, attributes }) => {
 	const {
-		query: { postType: toPostType },
+		query: { postType: queriedPostType },
 		showRelated,
-		fromPosts,
+		relationshipName,
+		sourcePosts,
 	} = attributes;
 
-	const { postTypes, currentPostId, hasPostRelationships } = useSelect(
+	const {
+		postTypes,
+		postTypeRelationships,
+		hasPostRelationships,
+		currentPostId,
+		currentPostType,
+	} = useSelect(
 		(select) => {
 			const { getPostTypes } = select(coreStore);
 			const excludedPostTypes = ['attachment'];
@@ -35,69 +50,116 @@ const BlockEdit = ({ setAttributes, attributes }) => {
 				({ viewable, slug }) => viewable && !excludedPostTypes.includes(slug),
 			);
 
-			const postId = select(editorStore).getCurrentPostId();
-			const postRelationships = select(store).getRelationships(fromPosts?.[0]?.id || postId);
+			const currentPostId = select(editorStore).getCurrentPostId();
+			const currentPostType = select(editorStore).getCurrentPostType();
+			const postRelationships = select(store).getRelationships(
+				sourcePosts?.[0]?.id || currentPostId,
+			);
 			const postTypeRelationships = Object.values(postRelationships).filter(
 				(relationship) =>
 					Array.isArray(relationship.post_type) &&
-					relationship.post_type.includes(toPostType),
+					relationship.post_type.includes(queriedPostType),
 			);
 
 			return {
 				postTypes: filteredPostTypes,
-				currentPostId: postId,
+				postTypeRelationships,
 				hasPostRelationships: postTypeRelationships.length > 0,
+				currentPostId,
+				currentPostType,
 			};
 		},
-		[toPostType, fromPosts?.length],
+		[queriedPostType, sourcePosts?.length],
 	);
 
-	const postTypeSlugs = useMemo(() => (postTypes || []).map(({ slug }) => slug), [postTypes]);
+	const postTypesSlugs = useMemo(() => (postTypes || []).map(({ slug }) => slug), [postTypes]);
+
+	const postTypeRelationshipsOptions = useMemo(
+		() =>
+			postTypeRelationships.map((relationship) => ({
+				value: relationship.rel_name,
+				label: relationship.labels.name,
+			})),
+		[postTypeRelationships],
+	);
+
+	const postTypeRelationshipsControlLabel = __('Relationship', 'tenup-content-connect');
+	const postTypeRelationshipsControlHelp = __(
+		'Select a relationship to determine how related items are retrieved.',
+		'tenup-content-connect',
+	);
+	const sourcePostsControlHelp = __(
+		'Choose the post from which related items will be pulled. Defaults to the current post.',
+		'tenup-content-connect',
+	);
+
+	const onPostTypeRelationshipChange = (value) => {
+		setAttributes({ relationshipName: value });
+	};
+
+	const resetAll = () => {
+		setAttributes({
+			showRelated: false,
+			sourcePosts: undefined,
+			relationshipName: undefined,
+		});
+	};
 
 	return (
 		<InspectorControls>
 			<PanelBody title={__('Related', 'tenup-content-connect')} initialOpen={false}>
 				<ToggleControl
 					label={__('Only show related items', 'tenup-content-connect')}
-					disabled={!hasPostRelationships}
 					checked={showRelated}
-					onChange={(value) =>
-						setAttributes({
-							showRelated: value,
-							sourcePostId: currentPostId,
-						})
-					}
-					help={__(
-						'When enabled, only items related to the current post will be displayed.',
-						'tenup-content-connect',
-					)}
-					__nextHasNoMarginBottom
+					onChange={(value) => {
+						if (!value) {
+							resetAll();
+						} else {
+							setAttributes({
+								showRelated: value,
+								sourcePosts: [
+									{
+										id: currentPostId,
+										type: currentPostType,
+										uuid: uuidv4(),
+									},
+								],
+							});
+						}
+					}}
 				/>
-				{!hasPostRelationships && (
+				{showRelated && (
+					<BaseControl help={sourcePostsControlHelp}>
+						<ContentPicker
+							onPickChange={(ids) =>
+								setAttributes({ sourcePosts: ids.length ? ids : undefined })
+							}
+							mode="post"
+							content={sourcePosts}
+							contentTypes={postTypesSlugs}
+							singlePickedLabel={__('Selected post:', 'tenup-content-connect')}
+							multiPickedLabel={__('Selected posts:', 'tenup-content-connect')}
+						/>
+					</BaseControl>
+				)}
+				{showRelated && postTypeRelationshipsOptions.length > 1 && (
+					<SelectControl
+						__nextHasNoMarginBottom
+						__next40pxDefaultSize
+						options={postTypeRelationshipsOptions}
+						value={relationshipName}
+						label={postTypeRelationshipsControlLabel}
+						onChange={onPostTypeRelationshipChange}
+						help={postTypeRelationshipsControlHelp}
+					/>
+				)}
+				{showRelated && !hasPostRelationships && (
 					<Notice spokenMessage={null} status="warning" isDismissible={false}>
 						{__(
-							'No relationships exist for the selected post type. Try selecting a different post type.',
+							'No relationships exist for the selected post type or post. Try selecting a different post or post type.',
 							'tenup-content-connect',
 						)}
 					</Notice>
-				)}
-				{showRelated && (
-					<>
-						<ContentPicker
-							onPickChange={(ids) =>
-								setAttributes({ fromPosts: ids.length ? ids : undefined })
-							}
-							mode="post"
-							content={fromPosts}
-							contentTypes={postTypeSlugs}
-						/>
-						<Notice spokenMessage={null} status="warning" isDismissible={false}>
-							{__(
-								'Select a different post as the source for related items. If none is selected, the current post will be used.',
-								'tenup-content-connect',
-							)}
-						</Notice>
-					</>
 				)}
 			</PanelBody>
 		</InspectorControls>
@@ -111,9 +173,11 @@ registerBlockExtension('core/query', {
 			type: 'boolean',
 			default: false,
 		},
-		fromPosts: {
+		sourcePosts: {
 			type: 'array',
-			default: [],
+		},
+		relationshipName: {
+			type: 'string',
 		},
 	},
 	classNameGenerator: () => '',
