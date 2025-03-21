2eb7565dd4e04f65f143d1e81cf59a903ba4b404
Sérgio Santos <me@s3rgiosan.com>
2025-03-21 09:55:39 +0000

    Remove dist files

--- dist/js/classic-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-deprecated', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => 'b947aa668d4c3974067b');
