bfc9d66d95f76794939dcd96ee5ab8dd786f0ade
Sérgio Santos <me@s3rgiosan.com>
2025-04-04 10:56:08 +0100

    Customize picked relationship item

--- assets/js/components/relationship-manager.tsx
+++ assets/js/components/relationship-manager.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import { ContentPicker } from '@10up/block-components';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { safeDecodeURI } from '@wordpress/url';
+import { decodeEntities } from '@wordpress/html-entities';
+import { __experimentalTruncate as Truncate } from '@wordpress/components';
 import { store } from '../store';
 import { ContentConnectRelationship } from '../store/types';
 
 type RelationshipManagerProps = {
 	postId: number | null;
 	relationship: ContentConnectRelationship;
+};
+
+type PickedRelationshipType = {
+	id: number;
+	type: string;
+	uuid: string;
+	title: string;
+	url: string;
+};
+
+/**
+ * Component to render a preview of a picked relationship.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {PickedRelationshipType} props.item - The picked relationship to display.
+ * @returns {*} React JSX
+ */
+const PickedRelationshipPreview: React.FC<{ item: PickedRelationshipType }> = ({ item }) => {
+	const { title, type, url = '' } = item;
+	return (
+		<>
+			{type !== 'user' ? (
+				<a href={safeDecodeURI(url) || ''} target="_blank" rel="noopener noreferrer">
+					<Truncate>{decodeEntities(title)}</Truncate>
+				</a>
+			) : (
+				<Truncate>{decodeEntities(title)}</Truncate>
+			)}
+		</>
+	);
 };
 
 export function RelationshipManager({ postId, relationship }: RelationshipManagerProps) {
@@ -32,6 +66,7 @@ export function RelationshipManager({ postId, relationship }: RelationshipManage
 			contentTypes={relationship?.post_type}
 			maxContentItems={relationship?.max_items ?? 100}
 			isOrderable={relationship?.sortable ?? false}
+			PickedItemPreviewComponent={PickedRelationshipPreview}
 		/>
 	);
 }
