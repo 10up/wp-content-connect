7cbb98dc27927eb37f2cbd88d2e5c5a725b3c846
Sérgio Santos <me@s3rgiosan.com>
2025-05-22 16:46:13 +0100

    Restore Search V1 endpoint

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -98,6 +98,7 @@ class Plugin {
 			new ClassicEditor(),
 			new BlockEditor(),
 			new DeletedItems(),
+			new API\V1\Search(),
 			new API\V2\Route\Relationships(),
 			new API\V2\Post\Route\Relationships(),
 			new API\V2\Post\Route\RelatedEntities(),
