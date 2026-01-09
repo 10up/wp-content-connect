58aa624a444fa71c12b27b2fb2bf9f2149ffc299
Sérgio Santos <me@s3rgiosan.com>
2026-01-09 20:14:43 +0000

    Fix REST tests

--- tests/integration/RESTTest.php
+++ tests/integration/RESTTest.php
@@ -23,8 +23,13 @@ class RESTTest extends ContentConnectTestCase {
 	public function setUp(): void {
 		parent::setUp();
 
+		\TenUp\ContentConnect\Plugin::instance();
+
 		$this->add_post_relations();
 		$this->add_user_relations();
+
+		$rest = new \TenUp\ContentConnect\REST();
+		$rest->add_links();
 	}
 
 	/**
@@ -33,12 +38,6 @@ class RESTTest extends ContentConnectTestCase {
 	 * @return void
 	 */
 	public function test_links_added_to_posts_with_show_in_rest() {
-		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-links' );
-		$registry->define_post_to_user( 'post', 'test-user-links' );
-
-		$post = get_post( 1 );
-
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
 		$data     = $response->get_data();
@@ -55,9 +54,6 @@ class RESTTest extends ContentConnectTestCase {
 	 * @return void
 	 */
 	public function test_links_include_relationships_link() {
-		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-rel-link' );
-
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
 		$links    = $response->get_links();
@@ -80,10 +76,6 @@ class RESTTest extends ContentConnectTestCase {
 	 * @return void
 	 */
 	public function test_links_include_related_links_for_each_relationship() {
-		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-related-link-1' );
-		$registry->define_post_to_post( 'post', 'post', 'test-related-link-2' );
-
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
 		$links    = $response->get_links();
@@ -111,9 +103,6 @@ class RESTTest extends ContentConnectTestCase {
 	 * @return void
 	 */
 	public function test_links_have_correct_href_format_for_relationships() {
-		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-href-format' );
-
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
 		$links    = $response->get_links();
@@ -137,8 +126,7 @@ class RESTTest extends ContentConnectTestCase {
 	 */
 	public function test_links_have_correct_href_format_for_related() {
 		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-related-href' );
-		$rel_key = $registry->get_relationship_key( 'post', 'post', 'test-related-href' );
+		$rel_key  = $registry->get_relationship_key( 'post', 'post', 'basic' );
 
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
@@ -151,29 +139,57 @@ class RESTTest extends ContentConnectTestCase {
 		$this->assertIsArray( $related_links );
 		$this->assertNotEmpty( $related_links );
 
-		$link = $related_links[0];
-		$this->assertArrayHasKey( 'href', $link );
-		$this->assertArrayHasKey( 'attributes', $link );
-		$href = $link['href'];
+		// Find the link for the 'basic' relationship
+		$basic_link = null;
+		foreach ( $related_links as $link ) {
+			if ( isset( $link['href'] ) && strpos( $link['href'], rawurlencode( $rel_key ) ) !== false ) {
+				$basic_link = $link;
+				break;
+			}
+		}
+
+		$this->assertNotNull( $basic_link, 'Should find link for basic relationship' );
+		$this->assertArrayHasKey( 'href', $basic_link );
+		$this->assertArrayHasKey( 'attributes', $basic_link );
+		$href = $basic_link['href'];
 		$this->assertStringContainsString( rest_url( '/content-connect/v2/post/1/related' ), $href );
-		$this->assertStringContainsString( 'rel_key=' . urlencode( $rel_key ), $href );
+		$this->assertStringContainsString( 'rel_key=' . rawurlencode( $rel_key ), $href );
 		$this->assertStringContainsString( 'rel_type=post-to-post', $href );
 	}
 
 	/**
-	 * Tests that no links are added when post has no relationships.
+	 * Tests that no links are added when post type has no registered relationships.
 	 *
 	 * @return void
 	 */
 	public function test_no_links_added_when_post_has_no_relationships() {
-		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
+		// Register a post type without any relationships
+		register_post_type(
+			'no-relationships',
+			array(
+				'public'       => true,
+				'show_in_rest' => true,
+			)
+		);
+
+		// Ensure REST routes are registered for the new post type
+		$server = rest_get_server();
+		do_action( 'rest_api_init', $server );
+
+		$post_id = $this->factory()->post->create(
+			array(
+				'post_type' => 'no-relationships',
+			)
+		);
+
+		$request  = new \WP_REST_Request( 'GET', '/wp/v2/no-relationships/' . $post_id );
 		$response = rest_do_request( $request );
-		$links    = $response->get_links();
 
 		$this->assertSame( 200, $response->get_status() );
+		$links = $response->get_links();
 
 		if ( isset( $links['content-connect:relationships'] ) ) {
-			$this->fail( 'Links should not be added when post has no relationships' );
+			$this->fail( 'Links should not be added when post type has no registered relationships' );
 		}
 	}
 
@@ -191,6 +207,9 @@ class RESTTest extends ContentConnectTestCase {
 			)
 		);
 
+		$server = rest_get_server();
+		do_action( 'rest_api_init', $server );
+
 		$post_id = $this->factory()->post->create(
 			array(
 				'post_type' => 'no-rest',
@@ -203,16 +222,8 @@ class RESTTest extends ContentConnectTestCase {
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/no-rest/' . $post_id );
 		$response = rest_do_request( $request );
 
-		// The endpoint itself might not exist (404) because show_in_rest=false,
-		// but if it does exist, links shouldn't be added.
-		if ( 200 === $response->get_status() ) {
-			$links = $response->get_links();
-			$this->assertArrayNotHasKey( 'content-connect:relationships', $links );
-		} else {
-			// If the endpoint doesn't exist, that's also valid - posts without show_in_rest
-			// typically don't have REST endpoints, so links can't be added.
-			$this->assertNotSame( 200, $response->get_status() );
-		}
+		$links = $response->get_links();
+		$this->assertArrayNotHasKey( 'content-connect:relationships', $links );
 	}
 
 	/**
@@ -221,11 +232,6 @@ class RESTTest extends ContentConnectTestCase {
 	 * @return void
 	 */
 	public function test_multiple_relationships_result_in_multiple_related_links() {
-		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-multi-1' );
-		$registry->define_post_to_post( 'post', 'post', 'test-multi-2' );
-		$registry->define_post_to_post( 'post', 'post', 'test-multi-3' );
-
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
 		$links    = $response->get_links();
@@ -235,7 +241,8 @@ class RESTTest extends ContentConnectTestCase {
 
 		$related_links = $links['content-connect:related'];
 		$this->assertIsArray( $related_links );
-		$this->assertGreaterThanOrEqual( 3, count( $related_links ) );
+		// Post 1 has relationships: 'basic' (post-to-post), 'complex' (post-to-post), 'owner' (post-to-user), 'contrib' (post-to-user)
+		$this->assertGreaterThanOrEqual( 4, count( $related_links ) );
 	}
 
 	/**
@@ -244,10 +251,6 @@ class RESTTest extends ContentConnectTestCase {
 	 * @return void
 	 */
 	public function test_both_post_to_post_and_post_to_user_relationships_included() {
-		$registry = get_registry();
-		$registry->define_post_to_post( 'post', 'post', 'test-both-p2p' );
-		$registry->define_post_to_user( 'post', 'test-both-p2u' );
-
 		$request  = new \WP_REST_Request( 'GET', '/wp/v2/posts/1' );
 		$response = rest_do_request( $request );
 		$links    = $response->get_links();
