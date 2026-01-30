ff0b7227f6063d358c899d4ab79b242bbeb7e027
Sérgio Santos <me@s3rgiosan.com>
2026-01-30 00:08:15 +0000

    Remove unused class properties

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -23,31 +23,6 @@ class Plugin {
 	public $registry;
 
 	/**
-	 * @var WPQueryIntegration
-	 */
-	public $wp_query_integration;
-
-	/**
-	 * @var UserQueryIntegration
-	 */
-	public $user_query_integration;
-
-	/**
-	 * @var MetaBox
-	 */
-	public $meta_box;
-
-	/**
-	 * @var Search
-	 */
-	public $search;
-
-	/**
-	 * @var DeletedItems
-	 */
-	public $deleted_items;
-
-	/**
 	 * The single instance of the class.
 	 *
 	 * @var Plugin
