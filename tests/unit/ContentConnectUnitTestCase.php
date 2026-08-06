6a50dda3564c08b03cc0574b72b709fd665feb26
Sérgio Santos <me@s3rgiosan.com>
2026-08-06 17:43:58 +0100

    Upgrade wp_mock to 1.x and migrate the unit suite

--- tests/unit/ContentConnectUnitTestCase.php
+++ tests/unit/ContentConnectUnitTestCase.php
@@ -2,26 +2,15 @@
 
 namespace TenUp\ContentConnect\Tests\Unit;
 
-class ContentConnectUnitTestCase extends \PHPUnit\Framework\TestCase {
+use WP_Mock\Tools\TestCase;
+
+class ContentConnectUnitTestCase extends TestCase {
 
 	public function setUp(): void {
-		\WP_Mock::setUp();
+		parent::setUp();
 
 		\WP_Mock::userFunction( 'plugin_dir_url', array( 'return' => 'https://contentconnect.test/wp-content/plugins/content-connect/' ) );
 		\WP_Mock::userFunction( 'wp_create_nonce', array( 'return' => '1234567890' ) );
-
-		parent::setUp();
-	}
-
-	public function tearDown(): void {
-		// Add assertions from Mockery to the total count
-		if ( $container = \Mockery::getContainer() ) {
-			$this->addToAssertionCount( $container->mockery_getExpectationCount() );
-		}
-
-		\WP_Mock::tearDown();
-
-		parent::tearDown();
 	}
 
 }
