879a039bc45568fe6ff6474fc0c82cf4cd6e7f45
Sérgio Santos <me@s3rgiosan.com>
2026-08-06 17:14:39 +0100

    Use dedicated loop variable in PostToPost::filter_data()

--- includes/UI/PostToPost.php
+++ includes/UI/PostToPost.php
@@ -47,11 +47,11 @@ class PostToPost extends PostUI {
 
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
-				$post = $query->next_post();
+				$related_post = $query->next_post();
 
 				$final_post = array(
-					'ID'   => $post->ID,
-					'name' => $post->post_title,
+					'ID'   => $related_post->ID,
+					'name' => $related_post->post_title,
 				);
 
 				$final_posts[] = apply_filters( 'tenup_content_connect_final_post', $final_post, $this->relationship );
